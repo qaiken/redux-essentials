d55e3a26bf19140b8cf4244573a8b3085c6d981a
Mehedi Hassan Piash <piash599@gmail.com>
2025-08-07 22:11:35 +0600

    - Fixing localization issue

--- src/app/home/_layout.tsx
+++ src/app/home/_layout.tsx
@@ -1,9 +1,11 @@
-import { ScrollView, Text } from 'react-native';
+import { ActivityIndicator, ScrollView, Text } from 'react-native';
 import { useLazyGetNowPlayingMovieQuery } from '@/src/service/query/rtkQuery';
 import { useEffect } from 'react';
 import AppBar from '@/src/components/base/appbar/AppBar';
+import { useLocalization } from '@/src/hooks/useLocalization';
 
 const HomeScreen = () => {
+  const localization = useLocalization();
   const [fetchNowPlayingMovie, { data, isFetching }] =
     useLazyGetNowPlayingMovieQuery();
 
@@ -11,10 +13,14 @@ const HomeScreen = () => {
     fetchNowPlayingMovie();
   }, []);
 
+  if (isFetching) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <AppBar title={'Home'}>
       <ScrollView>
-        <Text>Welcome to react native </Text>
+        <Text>Welcome to {localization.REACT_NATIVE()} </Text>
         <Text>{JSON.stringify(data, null, ' ')}</Text>
       </ScrollView>
     </AppBar>
